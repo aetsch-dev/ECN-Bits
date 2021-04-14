0be787a540eac1c41b3d135206637098f8ed4651
Jan Wollner <jan.wollner@telekom.de>
2021-04-14 14:02:44 +0200

    Change lib name

--- unity/Assets/Plugins/ECN-Bits/ECNBits/Android/ECNBits.Android.cs
+++ unity/Assets/Plugins/ECN-Bits/ECNBits/Android/ECNBits.Android.cs
@@ -1,3 +1,22 @@
+// Copyright © 2021
+//      Jan Wollner <jan.wollner@telekom.de>
+// Licensor: Deutsche Telekom
+//
+// Provided that these terms and disclaimer and all copyright notices
+// are retained or reproduced in an accompanying document, permission
+// is granted to deal in this work without restriction, including un‐
+// limited rights to use, publicly perform, distribute, sell, modify,
+// merge, give away, or sublicence.
+//
+// This work is provided “AS IS” and WITHOUT WARRANTY of any kind, to
+// the utmost extent permitted by applicable law, neither express nor
+// implied; without malicious intent or gross negligence. In no event
+// may a licensor, author or contributor be held liable for indirect,
+// direct, other damage, loss, or other issues arising in any way out
+// of dealing in the work, even if advised of the possibility of such
+// damage or existence of a defect, except proven that it results out
+// of said person’s immediate fault when using the work as intended.
+
 using System;
 using System.Net.Sockets;
 using System.Runtime.InteropServices;
@@ -6,7 +25,7 @@ namespace Plugins.ECNBits.Android
 {
     public static class Api
     {
-        private const string LIB_Name = "libecnbits-native";
+        private const string LIB_Name = "ecn-bitw";
 
         public static int PrepareSocket(Socket socket)
         {
